c8f65fa7767b0382c494dddce8abeffd87e7aea9
Larisa Bainton <larisabainton@gmail.com>
2023-10-05 22:04:05 -0400

    building index page

---
+++ src/components/contactForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContactForm = () => {
+    return (
+        <div className="contact-form"></div>
+    )
+}
+
+export default ContactForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import ContactForm from "../components/contactForm";
+
+const coverSection = (
+    <div className="index_cover">
+        <div className="index_cover_photo"></div>
+         <div className="index_cover_text"></div>
+    </div>
+)
+
+const aboutSection = (
+    <div className="index_about">
+        <div className="index_about_headshot"></div>
+        <div className="index_about_text"></div>
+    </div>
+);
+
+const calendarSection = (
+    <div className="index_calendar"></div>
+);
+
+const projectsSection = (
+    <div className="index_projects"></div>
+);
+
+const contactSection = (
+    <div className="index_contact">
+        <ContactForm />
+    </div>
+);
 
 const IndexPage = () => {
   return (
     <Layout>
-      <main>
-      </main>
+        <main className="index">
+            {coverSection}
+            {aboutSection}
+            {calendarSection}
+            {projectsSection}
+            {contactSection}
+        </main>
     </Layout>
-  )
+    )
 }
 
 export default IndexPage
