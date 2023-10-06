5361b416661a6d51c8e709a8e554446848f2a99f
Larisa Bainton <larisabainton@gmail.com>
2023-10-06 14:18:46 -0400

    add index styling and cover photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,30 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image";
+import '../style/main.scss';
+
 import Layout from "../components/layout"
+import Calendar from "../components/calendar";
 import ContactForm from "../components/contactForm";
 
 const coverSection = (
     <div className="index_cover">
-        <div className="index_cover_photo"></div>
+        <StaticImage className="index_cover_photo" src="../images/cover.jpg" alt="Larisa smiling and sitting on a bench" />
          <div className="index_cover_text"></div>
     </div>
 )
 
 const aboutSection = (
     <div className="index_about">
-        <div className="index_about_headshot"></div>
-        <div className="index_about_text"></div>
+        <div className="index_about_title">About</div>
+        <div className="index_about_text">Lorem Ipsum</div>
     </div>
 );
 
 const calendarSection = (
-    <div className="index_calendar"></div>
+    <div className="index_calendar">
+        <div className="index_calendar_title">Events</div>
+        <Calendar sourcePage="index" />
+    </div>
 );
 
 const projectsSection = (
