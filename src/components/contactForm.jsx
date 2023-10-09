99c6a1f9b44be51be2d016b8cce45d162bb251b2
Larisa Bainton <larisabainton@gmail.com>
2023-10-09 17:48:36 -0400

    style contact

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const ContactForm = () => {
     return (
-        <div className="contact-form"></div>
+        <div className="contact-form">
+            <form method="post" action="https://formspree.io/f/larisabainton@gmail.com">
+                <input type="text" aria-label="name" name="name" id="name" placeholder="Name" />
+                <input type="email" aria-label="email" name="email" id="email" placeholder="Email" />
+                <input type="text" aria-label="subject" name="subject" id="subject" placeholder="Subject" />
+                <textarea aria-label="message" name="message" id="message" rows="5" placeholder="Type your message here..."/>
+            <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
