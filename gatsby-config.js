35ff84c7690ae34eea20c29335742126bf5a9b5a
Larisa Bainton <larisabainton@gmail.com>
2023-10-31 20:15:19 -0400

    cherry pick out dependency upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
+      },
+    },
   ]
 };
